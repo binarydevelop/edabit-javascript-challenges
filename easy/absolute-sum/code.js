34536f55165e8d1e23ebee5ae131b86b74278611
Tushar Roy <binarydevelop@gmail.com>
2022-06-08 17:18:49 +0530

    add: solution for absolute-sum

--- easy/absolute-sum/code.js
+++ easy/absolute-sum/code.js
@@ -1,5 +1,10 @@
 function getAbsSum(arr) {
-  // Your code here.
+  let sum = arr[0] ;
+  arr.reduce((prev, curr) => {
+      sum += curr;
+      return sum;
+  })
+  return sum;
 }
 
 module.exports = getAbsSum;
